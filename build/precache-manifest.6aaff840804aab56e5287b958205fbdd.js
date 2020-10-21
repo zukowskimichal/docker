73d89081268ad51faa347bea3300a63f37d83bb6
zaklopotany <marry.jane.kelly@gmail.com>
2020-10-21 23:02:20 +0200

    all redundant files removed

---
+++ build/precache-manifest.6aaff840804aab56e5287b958205fbdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4388450cecc3d0c0a40b278f08558f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/js/main.23169dfd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
