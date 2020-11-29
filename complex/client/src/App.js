0e436a275c64c7ac049984e6544d22a735907199
zaklopotany <marry.jane.kelly@gmail.com>
2020-11-29 18:57:33 +0100

    small changes

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other Pageaaa</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
