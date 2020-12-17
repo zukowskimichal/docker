90ccec1a07470c85b33fd4f7628f1505da638329
zaklopotany <marry.jane.kelly@gmail.com>
2020-12-17 17:36:17 +0100

    complex k8s

--- old_complex/complex/client/src/App.js
+++ old_complex/complex/client/src/App.js
@@ -17,10 +17,10 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Learn React and kubernetes
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other Pageaaa</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
